47c8ab33e0b1560a32c2b1762a97a637889236d9
shubhi awasthi <119504808+shubhiawasthi13@users.noreply.github.com>
2025-08-06 00:01:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ export default function App() {
               "Express",
               "Bootstrap",
               "Tailwind",
-              "Threejs",
+              "Nextjs",
               "Python",
               "Django",
               "Php",
